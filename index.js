96c226e8fe64a8edb4047983d581a4f6471d4e97
k0bby69 <129067381+k0bby69@users.noreply.github.com>
2025-02-04 19:29:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,8 @@ const port = process.env.PORT || 8002;
 
 app.use(express.json());
 
-// Configure CORS to allow requests only from your React frontend
-const corsOptions = {
-  origin: ["https://webstore-frontend-9669.onrender.com", "https://webstore-userservice.onrender.com"],// Adjust this for production (e.g., https://yourfrontend.com)
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // If your app needs to send cookies or authorization headers
-};
-
-app.use(cors(corsOptions));  // Use the configured CORS
+// Remove CORS restrictions and allow requests from everywhere
+app.use(cors());
 
 app.use(express.static(__dirname + "/public"));
 
