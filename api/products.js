c024d573ecf12af0e21c9ac99dde383fafc5aeec
k0bby69 <129067381+k0bby69@users.noreply.github.com>
2025-02-07 20:51:18 +0000

    Add files via upload

--- api/products.js
+++ api/products.js
@@ -1,12 +1,13 @@
 const Product = require("../database/models/Product");
 const ProductService = require("../services/product-service");
-const { PublishMessage,SubscribeMessage } = require("../utils");
-const {auth,isSeller} = require("./middleware/auth");
-
+const { PublishMessage, SubscribeMessage } = require("../utils");
+const { auth, isSeller } = require("./middleware/auth");
+let print = console.log;
 productRoutes = (app, channel) => {
   const service = new ProductService();
-  SubscribeMessage(channel, service)
+  SubscribeMessage(channel, service);
 
+  //GET ALL PRODUCTS
   app.get("/", async (req, res, next) => {
     try {
       const { data } = await service.GetProducts();
@@ -16,11 +17,37 @@ productRoutes = (app, channel) => {
     }
   });
 
+  //  THIS ROUTE GETS THE PRODUCTS OF A SELLER BY ID. USED WHEN YOU ARE GOING TO THE PROFILE OF THE SELLER OF A PRODUCT
+  //LIKE WHEN YOU SEE A PRODUCT AND WANT TO SEE WHO IS SELLING
+  app.get("/seller-products/:id", async (req, res) => {
+    const seller = req.params.id;
+    const products = await Product.find({ seller });
 
-  app.post("/product/create",isSeller, async (req, res, next) => {
-    const { name, desc,img, type, stock, price, available } =
-      req.body;
-      const seller=req.user._id
+    res.status(200).json(products);
+  });
+  //THIS GETS THE PRODUCTS OF A SPECIFIC SELLER. LIKE WHEN YOU AS A SELLER WANTAS TO SEE ALL YOUR LISTED PRODUCTS
+  app.get("/products/seller", auth, async (req, res) => {
+    print("REQUEST USERRR", req.user);
+    const seller = req.user._id;
+    console.log(seller, "this is the seller id???????");
+    const products = await Product.find({ seller });
+
+    res.status(200).json(products);
+  });
+
+  app.post("/product/create", isSeller, async (req, res, next) => {
+    const {
+      name,
+      desc,
+      img,
+      type,
+      stock,
+      price,
+      available = true,
+      sizes = [],
+      colors = [],
+    } = req.body;
+    const seller = req.user._id;
     console.log(req.body);
     const { data } = await service.CreateProduct({
       name,
@@ -29,16 +56,36 @@ productRoutes = (app, channel) => {
       type,
       stock,
       price,
-      available, 
-      seller
+      available,
+      sizes,
+      colors,
+      seller,
     });
     return res.json(data);
   });
 
-  app.put("/product/:id", async (req, res) => {
+  //edit product
+
+  app.put("/product/:id", auth, async (req, res) => {
     const productId = req.params.id;
-    const { name, desc,img, type, stock, price, available } = req.body;
-    const seller=req.user._id
+    const {
+      name,
+      desc,
+      img = "",
+      type,
+      stock,
+      price,
+      available,
+      sizes = [],
+      colors = [],
+    } = req.body;
+    const product = await Product.findById(productId);
+    if (product.seller != req.user._id) {
+      res.status(403).json({
+        error: "Ah chale it is not your product why do you want to touch it?",
+      });
+    }
+    const seller = req.user._id;
 
     try {
       const { data } = await service.UpdateProduct(productId, {
@@ -48,10 +95,35 @@ productRoutes = (app, channel) => {
         type,
         stock,
         price,
-        available, 
+        available,
+        sizes,
+        colors,
         seller,
       });
 
+      const  payload  = await service.GetUpdatedProductPayload(
+        productId,
+        "UPDATE_CART_PRODUCT"
+      );
+      console.log("in update cart route");
+      console.log(payload.data);
+      const  wishlistPayload  = await service.GetUpdatedProductPayload(
+        productId,
+        "UPDATE_WISHLIST_PRODUCT"
+      );
+
+      PublishMessage(
+        channel,
+        process.env.CUSTOMER_BINDING_KEY,
+        JSON.stringify(payload.data)
+      );
+      PublishMessage(
+        channel,
+        process.env.SHOPPING_BINDING_KEY,
+        JSON.stringify(payload.data)
+      );
+
+      
       if (!data) {
         return res.status(404).json({ error: "Product not found" });
       }
@@ -62,18 +134,20 @@ productRoutes = (app, channel) => {
     }
   });
 
-  app.delete("/product/:id",isSeller, async (req, res, next) => {
-const productId =req.params.id
+  app.delete("/product/:id", isSeller, async (req, res, next) => {
+    const productId = req.params.id;
 
-const foundProduct=await Product.findById(productId)
-if(foundProduct.seller==req.user._id){
-  const deletedProduct=await Product.findByIdAndDelete(productId)
-  res.json(deletedProduct)
-}
-else{
-  res.json({"error":"you are not the seller how you want delete product? ah chale you no dey try"})
-}
+    const foundProduct = await Product.findById(productId);
+    if (foundProduct.seller == req.user._id) {
+      const deletedProduct = await Product.findByIdAndDelete(productId);
 
+      res.json(deletedProduct);
+    } else {
+      res.json({
+        error:
+          "you are not the seller how you want delete product? ah chale you no dey try",
+      });
+    }
   });
 
   app.get("/category/:type", async (req, res, next) => {
@@ -89,42 +163,70 @@ else{
 
   app.get("/:id", async (req, res, next) => {
     const productId = req.params.id;
+    print("gotten product id?????");
 
     try {
-      const { data } = await service.GetProductDescription(productId);
+      const data = await Product.findById(productId);
       return res.status(200).json(data);
     } catch (error) {
       return res.status(404).json({ error });
     }
   });
+  app.get("/search/:product", async (req, res, next) => {
+    try {
+      const product = req.params.product;
 
-  app.post("/ids", async (req, res, next) => {
-    const { ids } = req.body;
-    const products = await service.GetSelectedProducts(ids);
-    return res.status(200).json(products);
+      const products = await Product.find({
+        name: { $regex: product, $options: "i" },
+      });
+
+      res.status(200).json(products);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ msg: err.message });
+    }
   });
+
+  //ADD TO WISHLIST. SAME PUT ROUTE USED FOR BOTH ADDING AND REMOVING
+  //DID NOT REMOVE THE DELETE ROUTES FOR BOTH CART AND WISHLIST COS IDK IF THAT IS WHAT YOU USED
+  //DO NOT KNOW IF IT WILL WORK THOUGH BUT I USE THE PUT ROUTE BELOW FOR BOTH REMOVAL AND ADDING BY SIMPLY ADDING AND isRemove property set as true to remove and false to add to cart or wishlist
 
   app.put("/wishlist", auth, async (req, res, next) => {
     const { _id } = req.user;
-
     const { data } = await service.GetProductPayload(
       _id,
-      { productId: req.body._id },
-      "ADD_TO_WISHLIST"
+
+      {
+        productId: req.body.product._id,
+        amount: req.body.amount,
+      },
+
+      "ADD_TO_WISHLIST",
+      req.body.isRemove,
+      { sizes: req.body.product.sizes, colors: req.body.product.colors }
     );
+
+    console.log("in put wishlist route");
+    console.log(data);
+    print("WISHLIST DATA TO BE PUBLISHED", data);
 
     PublishMessage(
       channel,
       process.env.CUSTOMER_BINDING_KEY,
       JSON.stringify(data)
     );
+    PublishMessage(
+      channel,
+      process.env.SHOPPING_BINDING_KEY,
+      JSON.stringify(data)
+    );
+    console.log(data);
+    //old
+    // const response = { product: data.data.product, stock: data.data.qty };
+    //new
+    const response = { product: data.data.product, amount: req.body.amount };
 
-    res.status(200).json(data.data.product);
-    //data.data.product cos payload looks like this:
-    // const payload = {
-    //     event: event,
-    //     data: { userId, product, qty}
-    // };
+    res.status(200).json(response);
   });
 
   app.delete("/wishlist/:id", auth, async (req, res, next) => {
@@ -142,25 +244,34 @@ else{
       process.env.CUSTOMER_BINDING_KEY,
       JSON.stringify(data)
     );
-    // PublishMessage(
-    //   channel,
-    //   process.env.SHOPPING_BINDING_KEY,
-    //   JSON.stringify(data)
-    // );
+    PublishMessage(
+      channel,
+      process.env.SHOPPING_BINDING_KEY,
+      JSON.stringify(data)
+    );
 
-    res.status(200).json(data.data.product);
+    const response = { product: data.data.product, stock: data.data.qty };
+
+    res.status(200).json(response);
   });
+
+  //ADD TO CART. SAME PUT ROUTE USED FOR BOTH ADDING AND REMOVING
+  //DID NOT REMOVE THE DELETE ROUTES FOR BOTH CART AND WISHLIST COS IDK IF THAT IS WHAT YOU USED
+  //DO NOT KNOW IF IT WILL WORK THOUGH BUT I USE THE PUT ROUTE BELOW FOR BOTH REMOVAL AND ADDING BY SIMPLY ADDING AND isRemove property set as true to remove and false to add to cart or wishlist
 
   app.put("/cart", auth, async (req, res, next) => {
     const { _id } = req.user;
-    console.log('ADD TO CART PUT ROUTE')
-    console.log(req.body)
-
+    print(req.body);
+    print("ahhjk dwd", req.body.sizes, req.body.colors);
     const { data } = await service.GetProductPayload(
       _id,
       { productId: req.body.product._id, amount: req.body.amount },
-      "ADD_TO_CART"
+      "ADD_TO_CART",
+      req.body.isRemove,
+      { sizes: req.body.product.sizes, colors: req.body.product.colors }
     );
+    console.log("in put cart route");
+    console.log(data);
 
     PublishMessage(
       channel,
@@ -172,9 +283,11 @@ else{
       process.env.SHOPPING_BINDING_KEY,
       JSON.stringify(data)
     );
-console.log(data)
-
-    const response = { product: data.data.product, stock: data.data.qty };
+    console.log(data);
+    //old
+    // const response = { product: data.data.product, stock: data.data.qty };
+    //new
+    const response = { product: data.data.product, amount: req.body.amount };
 
     res.status(200).json(response);
   });
@@ -204,8 +317,6 @@ console.log(data)
 
     res.status(200).json(response);
   });
-
-
 };
 
 module.exports = productRoutes;
--- services/product-service.js
+++ services/product-service.js
@@ -1,6 +1,6 @@
 const ProductRepository = require("../database/repository/product-repository");
 const { FormatData } = require("../utils/index");
-
+let print=console.log
 class ProductService {
   constructor() {
     this.repository = new ProductRepository();
@@ -26,34 +26,37 @@ class ProductService {
     });
   }
   async UpdateProduct(productId, updatedData) {
+    
     const updatedProduct = await this.repository.UpdateProduct(productId, updatedData);
     return FormatData(updatedProduct);
 }
-
-  async GetProductDescription(productId) {
-    const product = await this.repository.FindById(productId);
-    return FormatData(product);
-  }
 
   async GetProductsByCategory(category) {
     const products = await this.repository.FindByCategory(category);
     return FormatData(products);
   }
 
-  async GetSelectedProducts(selectedIds) {
-    const products = await this.repository.FindSelectedProducts(selectedIds);
-    return FormatData(products);
-  }
 
-  async GetProductPayload(userId, { productId, amount }, event) {
+  async GetProductPayload(userId, { productId, amount }, event,isRemove,{sizes,colors}) {
     const product = await this.repository.FindById(productId);
-    console.log("FROM PRODUCT PAYLOAD TO BE SENT?????????", product);
+    print("AHHHH THE EMPTY SIZE OR COLOR?",sizes,colors)
+
+    product.sizes=sizes
+    product.colors=colors
+    if(product.sizes===undefined){
+      product.sizes=[]
+    }
+    if(product.colors===undefined){
+      product.colors=[]
+    }
 
     if (product) {
+      print("ABOUT TO SEND OAYLOADDDDDDD AND THE UPDATED SIZE X COLOR IS",product.sizes,product.colors)
       const payload = {
         event: event,
-        data: { userId, product, amount },
+        data: { userId, product, amount,isRemove },
       };
+      print("payload returned from get product payloaddddd",payload)
 
       return FormatData(payload);
     } else {
@@ -61,12 +64,31 @@ class ProductService {
     }
   }
 
+  async GetUpdatedProductPayload(productId, event) {
+    const product = await this.repository.FindById(productId);
+    print("GETING UPDATED PRODUCT PAYLOAD ",product)
+
+
+    if (product) {
+      const payload = {
+        event: event,
+        data: { productId,name:product.name, desc:product.desc,img:product.img,type:product.type,stock:product.stock,price:product.price,available:product.available},
+      };
+      print("payload returned from updated product payloaddddd",payload)
+
+      return FormatData(payload);
+    } else {
+      return FormatData({ error: "No product Available" });
+    }
+  }
+
+
   async reduceStock(data) {
     console.log('REDUCING STOCK??????')
     for (let i = 0; i < data.length; i++) {
       const product = await this.repository.FindById(data[i].productId);
       product.stock = product.stock - data[i].productAmountBought;
-      if (product.stock === 0) {
+      if (product.stock <= 0) {
         product.available = false;
       }
       await product.save()
@@ -74,6 +96,8 @@ class ProductService {
     }
 
   }
+
+
   async SubscribeEvents(payload) {
     payload = JSON.parse(payload);
     console.log("PAYLOAD IN SUBSCRIBING TO PRODUCT SERVICE");
@@ -84,6 +108,7 @@ class ProductService {
       case "REDUCE_PRODUCT_STOCK":
         this.reduceStock(data);
         break;
+
     }
 
   }
